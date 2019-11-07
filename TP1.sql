8af7c5d6c2e5be33dfd655ea737924e76255b364
carolinacouto98 <carolinacouto98@users.noreply.github.com>
2019-11-07 22:57:28 +0000

    falta ultima alinea, trigger, relatorio

--- TP1.sql
+++ TP1.sql
@@ -164,3 +164,12 @@ end
 go
 select * from dbo.Portfolio_List('Carolina Couto')
 drop function Portfolio_List
+go
+create view PORTFOLIO_SUMMARY as
+select PORTFOLIO.name, T1.NoInstruments, PORTFOLIO.totalval from (select name, count(isin) as NoInstruments from POSITION group by name) T1
+JOIN
+PORTFOLIO
+on T1.name = PORTFOLIO.name
+go
+select * from PORTFOLIO_SUMMARY
+drop view PORTFOLIO_SUMMARY
